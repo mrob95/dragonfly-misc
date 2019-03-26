d8bd105834d2904206ade3c0b12aadaa5abe7d97
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-03-26 01:10:37 +0000

    Music

---
+++ _music.py
@@ -0,0 +1,19 @@
+from dragonfly import Function, Choice, IntegerRef, Dictation, Repeat, MappingRule, Playback, Clipboard, Mimic, Key, Text
+
+class Music(MappingRule):
+    mapping = {
+        "volume up [<n>]"     : Key("volumeup/5:%(n)d"),
+        "volume down [<n>]"   : Key("volumedown/5:%(n)d"),
+        "volume (mute|unmute)": Key("volumemute"),
+        "music next"          : Key("tracknext"),
+        "music previous"      : Key("trackprev"),
+        "music (pause|play)"  : Key("playpause"),
+        }
+        extras = [
+            IntegerRef("n", 1, 20),
+        ]
+        defaults = {"n": 1}
+
+
+grammar.add_rule(Music())
+grammar.load()
